64377667562e8cc72be311fc826494b34612edd7
excelyno <excellentqweee@gmail.com>
2024-10-27 18:57:33 +0700

    deploy pertama kali

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 app.get('/names', (req, res) => {
     res.send("Hello, World");
 })
 
-app.listen(5000, () => {
-    console.log("server is running on port 5000");
+app.listen(8082, () => {
+    console.log("server is running on port 8082");
 })
